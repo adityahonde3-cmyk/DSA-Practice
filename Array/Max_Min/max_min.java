8b7b14c8e829691338c1082e3cf71c60315f9d9c
Your Name <you@example.com>
2026-05-29 14:31:20 +0530

    Implemented maximum element in array

---
+++ Array/Max_Min/max_min.java
@@ -0,0 +1,17 @@
+class Main {
+    public static void main(String[] args) {
+
+        int[] arr = {-10, -20, -3, -50};
+
+        int max = Integer.MIN_VALUE; 
+
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i] > max) {
+                max = arr[i];
+            }
+        }
+
+        System.out.println("Maximum element: " + max);
+    }
+}
+
--- Array/TwoSum.java
+++
@@ -1,7 +0,0 @@
-class TwoSum {
-
-    public static void main(String[] args) {
-
-        System.out.println("Two Sum Practice");
-    }
-}
