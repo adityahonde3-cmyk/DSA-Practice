2cb7a8e7554fe19bc49eaf6696a5af7d82981a21
Your Name <you@example.com>
2026-06-04 07:30:50 +0530

    completed sum code

---
+++ Array/sumofanarray.java
@@ -0,0 +1,8 @@
+class Main {
+    public static void main(String[] args) {
+  int [] arr={1,5,6,10,4,2,12};
+  int sum = 0;
+  for(int i= 0;i<arr.length;i++){
+  sum +=arr [i];  //Accumulation pattern
+  }
+  System.out.print(sum);
