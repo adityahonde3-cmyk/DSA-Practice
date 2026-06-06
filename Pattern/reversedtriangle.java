1ffd0e6293ed8cacc8225e2a53d129620bafc173
Your Name <you@example.com>
2026-06-06 09:01:04 +0530

    Reversed triangle completed

---
+++ Pattern/reversedtriangle.java
@@ -0,0 +1,5 @@
+class Solution {
+    public void pattern5(int n) {
+        for (int i=0;i<n;i++){        //only need of 
+            for (int j=0;j<n-i;j++){           
+                System.out.print("*");
